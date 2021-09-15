934c98653e1d7f2731a1ae7c78964f225ffdd880
susnitdgp <susnitdgp@gmail.com>
2021-09-15 07:30:00 +0000

    Logger Adding

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +12,14 @@ import org.springframework.ui.Model;
 @Controller
 public class DemoController {
 
+    Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/home")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+		
+		logger.info("Calling home endpoint");
+
 		return "greeting";
 	}
     

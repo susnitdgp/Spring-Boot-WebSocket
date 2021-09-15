e6629ef22cfe03a95f57879ceaf9e0441c808e03
susnitdgp <susnitdgp@gmail.com>
2021-09-15 06:53:14 +0000

    Second Commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class DemoController {
+
+
+   
+
+
+    @GetMapping("/home")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+    
+}
